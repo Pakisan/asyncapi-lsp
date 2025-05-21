a2f99ae44b4f7b5f2f8c4c28ccaf6815c22a7703
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2025-05-21 23:52:30 +0400

    feat(server): don't shutdown on LSP client disconnect

--- server/src/main/java/com/asyncapi/lsp/Main.java
+++ server/src/main/java/com/asyncapi/lsp/Main.java
@@ -6,41 +6,38 @@ import org.eclipse.lsp4j.launch.LSPLauncher;
 import org.eclipse.lsp4j.services.LanguageClient;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
 
 @Slf4j
 public class Main {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        log.info("AsyncAPI Language Server started");
+    public static void main(String[] args) {
+        final var serverPort = 5005;
         @NotNull final var languageServer = new AsyncAPILanguageServer();
 
-        try (@NotNull final var serverSocket = new ServerSocket(5005)) {
-            final var socket = serverSocket.accept();
-            @NotNull final var languageServerLauncher = serverLauncher(languageServer, socket)
-                    .startListening()
-                    .get();
-        } catch (IOException e) {
-            log.error("socket creation error - {}", e.getMessage());
+        try (@NotNull final var threadPool = Executors.newCachedThreadPool()) {
+            Callable<Void> callableTask =
+                    () -> {
+                        for (;;) {
+                            try (ServerSocket serverSocket = new ServerSocket(serverPort)) {
+                                Socket socket = serverSocket.accept();
+
+                                Launcher<LanguageClient> launcher = LSPLauncher.createServerLauncher(languageServer, socket.getInputStream(), socket.getOutputStream());
+                                launcher.startListening();
+                            }
+                        }
+                    };
+
+            threadPool.submit(callableTask);
+        } catch (Exception e) {
+            log.error("error - {}", e.getMessage());
             System.exit(1);
         } finally {
-            log.info("AsyncAPI Language Server stopped");
+            log.info("Exiting");
         }
-    }
-
-    @NotNull
-    public static Launcher<LanguageClient> serverLauncher(
-            @NotNull final AsyncAPILanguageServer languageServer,
-            @NotNull final Socket socket
-    ) throws IOException {
-        return LSPLauncher.createServerLauncher(
-                languageServer,
-                socket.getInputStream(),
-                socket.getOutputStream()
-        );
     }
 
 }
